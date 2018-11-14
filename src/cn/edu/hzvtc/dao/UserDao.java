9f43bd113385382bffd9aefdac9aa590d9b80694
TFC <TFC@DESKTOP-PJAN818>
2018-11-14 11:33:53 +0800

    创建数据表成功。

--- src/cn/edu/hzvtc/dao/UserDao.java
+++ src/cn/edu/hzvtc/dao/UserDao.java
@@ -1,6 +1,7 @@
 package cn.edu.hzvtc.dao;
+
 import cn.edu.hzvtc.entity.User;
 
 public interface UserDao extends BaseDao<User>{
-	
+	public User getManager(User employee);
 }
--- src/cn/edu/hzvtc/dao/impl/UserDaoImpl.java
+++ src/cn/edu/hzvtc/dao/impl/UserDaoImpl.java
@@ -1,10 +1,19 @@
 package cn.edu.hzvtc.dao.impl;
-
 import cn.edu.hzvtc.dao.UserDao;
+
 import cn.edu.hzvtc.entity.User;
+
 
 public class UserDaoImpl extends BaseDaoImpl<User> implements UserDao {
 
+	//登录方法
+	@Override
+	public User getManager(User user) {
+		
+		String hql = "FROM User u WHERE u.id = ? and u.password=?";
+		
+		return null;
+	}
+
 
 }
-
